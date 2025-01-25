17a3bd14bd5bd6a2ff9d61bb3dc1c1f78e72bc43
Alfred <ealfred828@gmail.com>
2025-01-25 10:27:45 +0100

    deploying

--- src/index.ts
+++ src/index.ts
@@ -15,8 +15,11 @@ app.use("/", userRouter);
 
 app.use(errorHandler);
 
-if (process.env.NODE_ENV !== "test") {
+// if (process.env.NODE_ENV !== "test") {
+//   app.listen(port, () => console.log(`Server started on ${port}`));
+// }
+
   app.listen(port, () => console.log(`Server started on ${port}`));
-}
+
 
 export default app;
